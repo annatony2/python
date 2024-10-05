a6aa13fea91cb0427e7433daea1027baa8e52532
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-05 23:42:13 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,12 @@
+#Author:Anna Tony
+#Date:05-10-2024
+# usage of arithmetic, comparison, and logical operators
+a=10
+b=5
+sum=a+b
+division=a/b
+print("Sum :",sum," ,division:",division)
+print("Is a greater than b ?:",a>b)
+print("Are a and b equal ?:",a==b)
+print("Logical and :",a>b and b<a)
+print("Logical or :",a<b or b<a)
