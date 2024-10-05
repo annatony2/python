706381f1e5976dd40771679196acb75b132369f9
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-06 00:38:50 +0530

    Add files via upload

---
+++ string_specific_format.py
@@ -0,0 +1,4 @@
+#Author:Anna Tony
+#Date:06-10-2024
+#print string in a specific format
+print("Twinkle, twinkle, little star,\n\t\t How I wonder what you are!\n\t\t\t\t Up above the world so high,\n\t\t\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t\t How I wonder what you are")
