c07bf850686fd36dd48371291ea7de40b0e449b6
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-06 00:50:27 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,9 @@
+#Author:Anna Tony
+#Date:05-10-2024
+#math module
+import math
+
+num=int(input("Enter a number :"))
+print("Square root of",num,"=",math.sqrt(num))
+print("Factorial of",num,"=",math.factorial(num))
+print(num,"raised to power of 2 =",math.pow(num,2))
