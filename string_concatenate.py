1ae1beeba2353c31f1f92509f3cfa9bec5c4e0d0
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-05 23:41:51 +0530

    Add files via upload

---
+++ string_concatenate.py
@@ -0,0 +1,6 @@
+#Author:Anna Tony
+#Date:05-10-2024
+#Extract a specific part of the string (substring) and then concatenate it with another string
+str1=("Hello, World! ")
+str2=str1[7:12]
+print(str2+" Everyone!")
