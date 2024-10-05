80001a02df33865eb6b5ca93735e514f388ccf4e
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-05 23:35:25 +0530

    Add files via upload

---
+++ name_age.py
@@ -0,0 +1,6 @@
+#Author:Anna Tony
+#Date:05-10-2024
+#asks the user to input their name and age
+name = input("Enter name: ")
+age = input("Enter age: ")
+print("Hello," + name + "! You are " + age + " years old.")
