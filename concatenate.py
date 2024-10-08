b2154ec14b821e4465fa451b3120f52ba343359f
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-08 15:32:55 +0530

    Add files via upload

---
+++ concatenate.py
@@ -0,0 +1,11 @@
+#author Anna Tony
+#date 08-10-2024
+#version 1
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+extracted_first_name=full_name[:length]
+print(extracted_first_name)
