8e024500f88c425a573d641a624438b4dcc8820d
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-15 16:09:32 +0530

    Add files via upload

---
+++ oddnumber_upto_n.py
@@ -0,0 +1,10 @@
+'''
+author Anna Tony
+date 15-10-2024
+python program to print odd numbers upto n
+'''
+limit=int(input("enter the limit : "))
+odd_number=1
+while odd_number<=limit:
+    print(odd_number,"\t",end="")
+    odd_number+=2
