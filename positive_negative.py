bc44aed13c1f7993333434f7ae50c036fd3c2f1a
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-10-15 14:04:30 +0530

    Add files via upload

---
+++ positive_negative.py
@@ -0,0 +1,10 @@
+'''
+author Anna Tony
+date 15-10-2024
+python program to check whether the given number is positive or not
+'''
+number=int(input("Enter a number :"))
+if number>0:
+    print("The given number",number,"is positive")
+else:
+    print("The given number", number, "is negative")
