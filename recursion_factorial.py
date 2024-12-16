3c597498e3b77f58e021d78c7b26d4456543ddb3
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-12-16 22:18:40 +0530

    Update recursion_factorial.py

--- recursion_factorial.py
+++ recursion_factorial.py
@@ -1,9 +1,10 @@
 #author Anna Tony
 #date 03-12-2024
 #version 1
-def factorial (n):
-    if n==1:
+def factorial(num):
+    if num==1:
         return 1
     else:
-        return n*factorial(n-1)
-print('Factorial : ',factorial(5))
+        return num*factorial(num-1)
+num=int(input('enter the number : '))
+print('Factorial : ',factorial(num))
